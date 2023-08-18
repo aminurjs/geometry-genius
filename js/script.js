2e5affb82cbf9a00e64bbb6f7433cb8aeda9e263
Aminur Rahman <dev.aminur@gmail.com>
2023-08-19 01:48:53 +0600

    Double click button added

--- js/script.js
+++ js/script.js
@@ -55,17 +55,26 @@ function areaCalculator(areaType, area){
     const count = calculationArea.childElementCount;
     const p = document.createElement('p');
     p.classList.add('py-3');
-    p.innerHTML = `${count + 1}. ${areaType} <span id="element${count}">${area}</span> cm<sup>2</sup> <button class="btn btn-secondary btn-sm ml-2" onclick="clicked(${count})"> Convert m<sup>2</sup></button> <br> <span class="ml-5 my-2 font-medium block" id="set-${count}"></span>`;
-
+    p.innerHTML = `${count + 1}. ${areaType} <span id="element${count}">${area}</span><span id="mcm${count}" class="font-medium"> cm<sup>2</sup></span>  <button id="convert-btn-${count}" class="btn btn-secondary btn-sm ml-2 mt-2" onclick="clicked(${count})"> Convert <span id="btn-${count}">m</span><sup>2</sup></button>`;
     calculationArea.appendChild(p);
 }
 
 function clicked(num){
+    const btn = document.getElementById('btn-' + num)
     const text = document.getElementById('element' + num);
     const textString = text.innerText;
     const number = parseFloat(textString);
-    const mitre = number / 100;
-
-    const set = document.getElementById('set-' + num);
-    set.innerHTML = `Element area :  ${mitre} m<sup>2</sup>`;
+    const set = document.getElementById('mcm' + num);
+    if (btn.innerText !== 'M'){
+        const mitre = number * 100;
+        text.innerText = mitre;
+        btn.innerText = 'm';
+        set.innerHTML = ` cm<sup>2</sup>`;
+    }
+    else if(btn.innerText === 'M'){   
+        const mitre = number / 100;
+        text.innerText = mitre;
+        btn.innerText = 'cm';
+        set.innerHTML = ` m<sup>2</sup>`;
+    }
 }
